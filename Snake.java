e9464c6ff26d306bf97c8e375351db01e553d5ff
Keith Radford <53020925+keithradford@users.noreply.github.com>
2019-09-14 13:43:02 -0700

    Added game over and score
    Game over when snake collides with wall, still working on when snake collides with itself.

--- Snake.java
+++ Snake.java
@@ -3,12 +3,14 @@ public class Snake{
     private Node head;
     private Node tail;
     private int size;
+    private int score;
 
     public Snake(){
         head = null;
         tail = null;
         size = 1;
-    }
+        score = 0;
+    }//constructor
 
     public Node getHead(){
         return head;
@@ -18,8 +20,23 @@ public class Snake{
         return tail;
     }
 
+    public int getScore(){
+        return this.score;
+    }
+
     public void sizeUp(){
         this.size += 1;
+        this.score = (this.score*(this.size/2)) + 10;
+    }//let Snake class know it got bigger
+
+    public void gameOver(){
+        int sc = this.score;
+        System.out.println("--------------------");
+        System.out.println("     GAME OVER!     ");
+        System.out.println("--------------------");
+        System.out.println("You finished with a score of: "+sc+"!");
+        System.out.println("Rerun the code to try again!");
+        System.exit(0);
     }
 
     public void enqueue(int x, int y){
@@ -32,7 +49,7 @@ public class Snake{
           tmp.next = tail;
           tail = tmp;
         }
-    }
+    }//enqueue new coordinates
 
     public void move(int x, int y){
         Node tmp = new Node(x, y);
@@ -52,43 +69,53 @@ public class Snake{
     }
 
     public void render(Border game){
+        if(head.getX() == 0 || head.getX() == game.getWidth() -1)
+            gameOver();
+        
+        else if(head.getY() == 0 || head.getY() == game.getHeight() -1)
+            gameOver();
+
         if(size == 1){
             game.snake(head.getX(), head.getY());
         }
         else{
             Node tmp = tail;
             for(int i = 0; i < size; i++){
-                game.snake(tmp.getX(), tmp.getY());
-                tmp = tmp.next;
+               // if((head.getX() == tmp.getX()) && head.getY() == tmp.getY())
+                   // gameOver();
+               // else{
+                    game.snake(tmp.getX(), tmp.getY());
+                    tmp = tmp.next;
+                }
             }
         }
-    }
+  //  }
 
     public void right(Border game, int prevX, int prevY){
         int x = (prevX + 1);
         move(x, prevY);
         dequeue(game);
         render(game);
-    }
+    }//move right
 
     public void left(Border game, int prevX, int prevY){
         int x = (prevX - 1);
         move(x, prevY);
         dequeue(game);
         render(game);
-    }
+    }//move left
 
     public void up(Border game, int prevX, int prevY){
         int y = (prevY - 1);
         move(prevX, y);
         dequeue(game);
         render(game);
-    }
+    }//move up
 
     public void down(Border game, int prevX, int prevY){
         int y = (prevY + 1);
         move(prevX, y);
         dequeue(game);
         render(game);
-    }
+    }//move down
 }
