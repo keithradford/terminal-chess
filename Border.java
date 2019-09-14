23cfc639504341af74fc3faad3568e602e710d35
Keith Radford <53020925+keithradford@users.noreply.github.com>
2019-09-14 13:38:37 -0700

    Added score under border

--- Border.java
+++ Border.java
@@ -8,6 +8,14 @@ public class Border {
         this.height = height;
         this.width = width;
         this.board = new char[this.width][this.height];
+    }//constructor
+
+    public int getHeight(){
+        return this.height;
+    }
+
+    public int getWidth(){
+        return this.width;
     }
 
     public void border() {
@@ -23,9 +31,9 @@ public class Border {
                     this.board[x][y] = ' ';
             }
         }
-    }
+    }//algorithm to create outline
 
-    public void render() {
+    public void render(Snake s) {
         for (int y = 0; y < this.height; y++) {
             for (int x = 0; x < this.width; x++) {
                 if (x == 0)
@@ -34,18 +42,19 @@ public class Border {
                     System.out.print(this.board[x][y]);
             }
         }
-    }
+        System.out.println("\nScore: "+s.getScore());
+    }//prints border
 
     public void replace(int x, int y, char ch){
         this.board[x][y] = ch;
-    }
+    }//replace location with new char
 
     public void snake(int x, int y){
         this.board[x][y] = 'X';
-    }
+    }//place snake
 
     public void food(int x, int y){
         this.board[x][y] = 'O';
-    }
+    }//place food
  
 } 
